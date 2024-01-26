678cb3a61eabed8a9f14d89410ca2c035c569dc9
Wizard Today <aleshko.vitali@gmail.com>
2024-01-27 02:38:56 +0300

    deploy

--- server.ts
+++ server.ts
@@ -2,24 +2,20 @@ import { main } from './main.ts'
 
 const port = 8080
 
-// const handler = async (request: Request): Promise<Response> => {
-//   try {
-//     if (request.method == 'POST') {
-//       const input = await request.text()
-//       const result = await main(JSON.parse(input))
-//       const output = JSON.stringify(result)
-//       console.log(input, output)
-//       return new Response(output)
-//     }
-//   } catch (error) {
-//     console.error(error)
-//     return new Response(null, { status: 500 })
-//   }
-//   return new Response(null, { status: 404 })
-// }
-
 const handler = async (request: Request): Promise<Response> => {
-  return new Response('Surprise!!!', { status: 200 })
+  try {
+    if (request.method == 'POST') {
+      const input = await request.text()
+      const result = await main(JSON.parse(input))
+      const output = JSON.stringify(result)
+      console.log(input, output)
+      return new Response(output)
+    }
+  } catch (error) {
+    console.error(error)
+    return new Response(null, { status: 500 })
+  }
+  return new Response(null, { status: 404 })
 }
 
 declare const Deno: any
