7bff5dc197f02ce2722a873980e1d043530b6f12
Wizard Today <aleshko.vitali@gmail.com>
2024-01-28 02:43:24 +0300

    test fix

--- server/serve.ts
+++ server/serve.ts
@@ -1,8 +1,6 @@
 import { Actions } from './types.ts'
 import { handler } from './handler.ts'
 
-declare const Deno: any
-
 export const startServer = (run_action: Actions) => {
   Deno.serve({ port: 8080 }, handler(run_action))
   console.log('Server started!')
